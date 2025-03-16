a7fae01d392cbaaa9031be04bdcb4b60de2fc5db
aditya <adityavittal92@gmail.com>
2025-03-16 10:27:51 +0530

    test

---
+++ misc/vault-secrets/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "s3" {
+	bucket = "terraformaddy"
+	key    = "vaultsecrets/state"
+	region = "us-east-1"
+  }
+}
+
+provider "vault" {
+  address= "http://vault-public.doubtfree.online:8200"
+  token = var.vault_token
+
+}
+variable "vault_token" {}
+
+#store the secrets
+resource "vault_generic_secret" "ssh" {
+ path = "infra/ssh"
+ data_json = <<EOT
+{
+	"username": "ec2-user",
+	"password": "DevOps321"
+}
+EOT
+}
