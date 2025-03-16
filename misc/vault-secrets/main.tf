b4ce2f0b8e1efc5c3f841b2f0fefbdcd2008aca3
aditya <adityavittal92@gmail.com>
2025-03-16 12:12:10 +0530

    test

--- misc/vault-secrets/main.tf
+++ misc/vault-secrets/main.tf
@@ -35,3 +35,18 @@ resource "vault_generic_secret" "ssh" {
 }
 EOT
 }
+resource "vault_mount" "roboshop-dev" {
+  path = "roboshop-dev"
+  type = "kv"
+  options = { version = "2" }
+  description = "Infra secrets"
+}
+resource "vault_generic_secret" "roboshop-dev-cart" {
+  path = "${vault_mount.roboshop-dev.path}/cart"
+  data_json = <<EOT
+{
+	"username": "ec2-user",
+	"password": "DevOps321"
+}
+EOT
+}
